63703e47bacf72b2c2747860e1ead0fb436f54fd
achroma <alinachroma@gmail.com>
2024-03-14 18:39:45 +0100

    added ViewModel
    ViewModel has the logic responsible for the state updates

---
+++ app/src/main/java/com/example/berlin_bucket_list/ui/theme/BerlinBucketListViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.berlin_bucket_list.ui.theme
+
+import androidx.lifecycle.ViewModel
+import com.example.berlin_bucket_list.R
+import com.example.berlin_bucket_list.data.BerlinBucketListUiState
+import com.example.berlin_bucket_list.data.DataSource
+import com.example.berlin_bucket_list.model.CategoryType
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class BerlinBucketListViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(BerlinBucketListUiState())
+    val uiState: StateFlow<BerlinBucketListUiState> = _uiState.asStateFlow()
+
+    fun updateCurrentCategory(clickedCategory: CategoryType) {
+        _uiState.update {
+            it.copy(
+                currentCategory = clickedCategory,
+                placesToShow = when (clickedCategory.title) {
+                    R.string.category_1 -> DataSource.cafes
+                    R.string.category_2 -> DataSource.parks
+                    R.string.category_3 -> DataSource.shoppingCenters
+                    R.string.category_4 -> DataSource.kidsFriendly
+                    else -> emptyList()
+                }
+            )
+        }
+    }
+}
