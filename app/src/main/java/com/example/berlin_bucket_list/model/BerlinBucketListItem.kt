b71b73018bc062268e6584daa42afc15884d2ed8
achroma <alinachroma@gmail.com>
2024-03-14 16:16:10 +0100

    refactoring - move data to BerlinBucketListItem
    Both Home screen and Recommendations screen contains items (cards) that have exactly the same structure, so instead of having Category and BerlinBucketListItem as separate data classes, it makes sense to have just the BerlinBucketListItem with nullable properties - it allows us to avoid code duplication.
    
    This way we can use BerlinBucketListItem for the elements in all the app screens - the structure of the elements is identical in almost all of them, so this is acceptable for this application.

---
+++ app/src/main/java/com/example/berlin_bucket_list/model/BerlinBucketListItem.kt
@@ -0,0 +1,14 @@
+package com.example.berlin_bucket_list.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class BerlinBucketListItem(
+    @DrawableRes val imageId: Int,
+    @StringRes val name: Int,
+    @StringRes val address: Int? = null,
+    @StringRes val credits: Int? = null,
+    val placeDescription: String? = "",
+    val placeDetails: String? = "",
+    val categoryType: CategoryType,
+)
--- app/src/main/java/com/example/berlin_bucket_list/model/Category.kt
+++
@@ -1,9 +0,0 @@
-package com.example.berlin_bucket_list.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Category(
-    @StringRes val categoryName: Int,
-    @DrawableRes val categoryImage: Int
-)
