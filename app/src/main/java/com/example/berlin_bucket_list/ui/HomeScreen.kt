5c21f45b52bbd7522b212b74c88a440c06b55209
achroma <alinachroma@gmail.com>
2024-04-15 12:30:24 +0200

    make an item card semi transparent

--- app/src/main/java/com/example/berlin_bucket_list/ui/HomeScreen.kt
+++ app/src/main/java/com/example/berlin_bucket_list/ui/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.example.berlin_bucket_list.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,24 +12,42 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.ClipOp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.addOutline
+import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -73,47 +93,126 @@ fun BerlinBucketListApp(
         else -> ""
     }
 
-    Scaffold(
-        topBar = {
-            BerlinBucketListAppBar(
-                screenTitle = topBarTitle ?: "",
-                canNavigateBack = navController.previousBackStackEntry != null,
-                navigateUp = { navController.navigateUp() }
-            )
-        }
-    ) { innerPadding ->
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.background_1),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.matchParentSize()
+        )
+        Scaffold(
+            containerColor = Transparent,
+            topBar = {
+                BerlinBucketListAppBar(
+                    screenTitle = topBarTitle ?: "",
+                    canNavigateBack = navController.previousBackStackEntry != null,
+                    navigateUp = { navController.navigateUp() }
+                )
+            },
+            modifier = Modifier
+        ) { innerPadding ->
 
-        NavHost(
-            navController = navController,
-            startDestination = Screen.HomeScreen.route,
-            modifier = modifier.padding(innerPadding)
-        ) {
-            composable(route = Screen.HomeScreen.route) {
-                HomeScreen(
-                    categories = DataSource.categories,
-                    onSelectionChanged = {
-                        viewModel.updateCurrentCategory(
-                            clickedCategory = it.categoryType
+            NavHost(
+                navController = navController,
+                startDestination = Screen.HomeScreen.route,
+                modifier = modifier.padding(innerPadding)
+            ) {
+                composable(route = Screen.HomeScreen.route) {
+                    HomeScreen(
+                        categories = DataSource.categories,
+                        onSelectionChanged = {
+                            viewModel.updateCurrentCategory(
+                                clickedCategory = it.categoryType
+                            )
+                            navController.navigate(Screen.RecommendationsScreen.route)
+                        },
+                        modifier = Modifier.padding(innerPadding),
+                    )
+                }
+                composable(route = Screen.RecommendationsScreen.route) {
+                    RecommendationsScreen(
+                        recommendedPlaces = uiState.placesToShow,
+                        onSelectionChanged = {
+                            viewModel.updateRecommendedPlace(recommendedPlace = it)
+                            navController.navigate(route = "Details/${it}")
+                        },
+                    )
+                }
+                composable(route = Screen.DetailsScreen.route) {
+                    DetailsScreen(
+                        item = uiState.recommendedPlace ?: emptyItem
+                    )
+                }
+            }
+        }
+    }
+}
+
+// The ClippedShadow and ShadowBox Composables fix the
+// material shadow glitch on transparent/translucent Composables.
+// Author: zed-alpha. Repo: https://gist.github.com/zed-alpha/3dc931720292c1f3ff31fa6a130f52cd
+@Composable
+fun ClippedShadow(elevation: Dp, shape: Shape, modifier: Modifier = Modifier) {
+    Layout(
+        modifier
+            .drawWithCache {
+                val path = androidx.compose.ui.graphics.Path()
+                var lastSize: Size? = null
+
+                fun updatePathIfNeeded() {
+                    if (size != lastSize) {
+                        path.reset()
+                        path.addOutline(
+                            shape.createOutline(size, layoutDirection, this)
                         )
-                        navController.navigate(Screen.RecommendationsScreen.route)
-                    },
-                    modifier = Modifier.padding(innerPadding),
-                )
-            }
-            composable(route = Screen.RecommendationsScreen.route) {
-                RecommendationsScreen(
-                    recommendedPlaces = uiState.placesToShow,
-                    onSelectionChanged = {
-                        viewModel.updateRecommendedPlace(recommendedPlace = it)
-                        navController.navigate(route = "Details/${it}")
+                        lastSize = size
                     }
-                )
+                }
+
+                onDrawWithContent {
+                    updatePathIfNeeded()
+                    clipPath(path, ClipOp.Difference) {
+                        this@onDrawWithContent.drawContent()
+                    }
+                }
             }
-            composable(route = Screen.DetailsScreen.route) {
-                DetailsScreen(
-                    item = uiState.recommendedPlace ?: emptyItem
-                )
+            .shadow(elevation, shape)
+    ) { _, constraints ->
+        layout(constraints.minWidth, constraints.minHeight) {}
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+@Composable
+fun ShadowBox(
+    elevation: Dp,
+    shape: Shape,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        {
+            ClippedShadow(elevation, shape, modifier)
+            CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
+                content()
             }
+        },
+        modifier
+    ) { measurable, constraints ->
+        require(measurable.size == 2)
+
+        val shadow = measurable[0]
+        val target = measurable[1]
+
+        val targetPlaceable = target.measure(constraints)
+        val width = targetPlaceable.width
+        val height = targetPlaceable.height
+
+        val shadowPlaceable = shadow.measure(Constraints.fixed(width, height))
+
+        layout(width, height) {
+            shadowPlaceable.place(0, 0)
+            targetPlaceable.place(0, 0)
         }
     }
 }
@@ -125,26 +224,39 @@ fun BerlinBucketListItem(
     onItemClicked: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    ElevatedCard(
-        onClick = onItemClicked,
+    ShadowBox(
+        elevation = 10.dp,
+        shape = Shapes.small,
         modifier = modifier
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
+        Card(
+            onClick = onItemClicked,
+            shape = Shapes.small,
+            colors = CardDefaults.elevatedCardColors(White.copy(alpha = 0.3f)),
+            modifier = modifier
         ) {
-            Box(
-                modifier = Modifier.size(dimensionResource(id = R.dimen.icon_height))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
             ) {
-                Image(
-                    painter = painterResource(id = item.imageId),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = modifier.clip(Shapes.small)
+                Box(
+                    modifier = Modifier.size(dimensionResource(id = R.dimen.icon_height))
+                ) {
+                    Image(
+                        painter = painterResource(id = item.imageId),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = modifier.clip(Shapes.small)
+                    )
+                }
+                Text(
+                    text = stringResource(id = item.name).uppercase(),
+                    style = MaterialTheme.typography.displayMedium,
+                    color = White
                 )
+
             }
-            Text(text = stringResource(id = item.name))
         }
     }
 }
