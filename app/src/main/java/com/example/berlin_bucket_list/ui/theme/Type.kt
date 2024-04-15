8da5329dc0516c9ee988d1a0e5701b185c52c892
achroma <alinachroma@gmail.com>
2024-04-15 12:31:46 +0200

    add the font

--- app/src/main/java/com/example/berlin_bucket_list/ui/theme/Type.kt
+++ app/src/main/java/com/example/berlin_bucket_list/ui/theme/Type.kt
@@ -2,11 +2,16 @@ package com.example.berlin_bucket_list.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.berlin_bucket_list.R
 
-// Set of Material typography styles to start with
+val AllertaStencilFamily = FontFamily(
+    Font(R.font.allerta_stencil_regular, FontWeight.Normal)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -14,8 +19,13 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = AllertaStencilFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp
     )
-    /* Other default text styles to override
+ /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
