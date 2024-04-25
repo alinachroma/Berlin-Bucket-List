33d67d84fec455dcf53ae0407888a1b29ac96947
achroma <alinachroma@gmail.com>
2024-04-25 16:56:59 +0200

    show composables depending on the content type
    Either LIST or LIST_AND_DETAILS, depending on if the screen is Compact, Medium or Expanded

--- app/src/main/java/com/example/berlin_bucket_list/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/berlin_bucket_list/ui/screens/HomeScreen.kt
@@ -11,3 +11,51 @@
 
         else -> BerlinBucketListContentType.LIST_ONLY
     }
+                    if (contentType == BerlinBucketListContentType.LIST_ONLY) {
+                        RecommendationsScreen(
+                            recommendedPlaces = uiState.placesToShow,
+                            onSelectionChanged = {
+                                viewModel.updateRecommendedPlace(recommendedPlace = it)
+                                navController.navigate(route = "Details/${it}")
+                            },
+                        )
+                    } else {
+                        RecommendedPlaceListAndDetails(
+                            placesToShow = uiState.placesToShow,
+                            onSelectionChanged = {
+                                viewModel.updateRecommendedPlace(recommendedPlace = it)
+                            },
+                            imageId = uiState.recommendedPlace?.imageId
+                                ?: R.drawable.appbar_black,
+                            placeDescription = uiState.recommendedPlace?.placeDescription ?: "",
+                            extraInfo = uiState.recommendedPlace?.extraInfo ?: "",
+                            address = uiState.recommendedPlace?.address ?: -1,
+                            credits = uiState.recommendedPlace?.credits ?: -1,
+                            modifier = Modifier,
+                            windowSize = windowSize
+                        )
+                    }
+                }
+                    if (contentType == BerlinBucketListContentType.LIST_ONLY) {
+                        DetailsScreen(
+                            windowSize = windowSize,
+                            item = uiState.recommendedPlace ?: emptyItem
+                        )
+                    } else {
+                        RecommendedPlaceListAndDetails(
+                            placesToShow = uiState.placesToShow,
+                            onSelectionChanged = {
+                                viewModel.updateRecommendedPlace(recommendedPlace = it)
+                            },
+                            imageId = uiState.recommendedPlace?.imageId
+                                ?: R.drawable.appbar_black,
+                            placeDescription = uiState.recommendedPlace?.placeDescription ?: "",
+                            extraInfo = uiState.recommendedPlace?.extraInfo ?: "",
+                            address = uiState.recommendedPlace?.address ?: -1,
+                            credits = uiState.recommendedPlace?.credits ?: -1,
+                            modifier = Modifier,
+                            windowSize = windowSize
+                        )
+                    }
+                }
+            }
